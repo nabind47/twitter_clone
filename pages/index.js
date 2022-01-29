58b318a745bb56641d892749e364d1be4c506c3a
creative201347 <creative201347@gmail.com>
2022-01-29 13:00:34 +0545

    basic functions completed

--- pages/index.js
+++ pages/index.js
@@ -19,27 +19,17 @@ export default function Home({ providers }) {
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
         <Feed />
-        {/* <Widgets /> */}
-        {/* Modal  */}
       </main>
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
-  // const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
-  //   (res) => res.json()
-  // );
-  // const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
-  //   (res) => res.json()
-  // );
   const providers = await getProviders();
   const session = await getSession(context);
 
   return {
     props: {
-      // trendingResults,
-      // followResults,
       providers,
       session,
     },
